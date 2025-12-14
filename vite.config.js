30fabfa5a75edc7af15b279599e0603f1869659e
andres <andres@n-dream.com>
2025-12-14 16:00:34 +0100

    v0.99 All files deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
+import { fileURLToPath } from 'node:url'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/ahead/',
   build: {
     outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        report: resolve(__dirname, 'report.html'),
+        actionPlan: resolve(__dirname, 'action_plan.html'),
+      },
+    },
   },
   plugins: [react()],
 })
